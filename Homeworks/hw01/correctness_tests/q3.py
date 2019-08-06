493aa647eee945534f7eca72caf2a52f918586e7
roberttwomey <robert.twomey@gmail.com>
2019-08-06 04:30:45 -0700

    added day 1 materials

---
+++ Homeworks/hw01/correctness_tests/q3.py
@@ -0,0 +1,24 @@
+test = {
+  'name': 'Question 3_',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> names_q1
+          2
+          >>> names_q2
+          4
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
