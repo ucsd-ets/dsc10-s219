918fb56b0b358b5a9529c780cfed869a659e50cf
Yunkai Chen <yuc331@ucsd.edu>
2019-08-10 11:34:25 -0700

    Making hw03

---
+++ Homeworks/hw03/tests/q1_2.py
@@ -0,0 +1,24 @@
+test = {
+  'name': 'Question 1_2',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> isinstance(least_volume, str)
+          True
+          >>> isinstance(most_volume, str)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ Homeworks/hw03/tests/q2_3.py
@@ -0,0 +1,24 @@
+test = {
+  'name': 'Question 2_3',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> isinstance(worst_apps, np.ndarray)
+          True
+          >>> len(worst_apps) == 5
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
