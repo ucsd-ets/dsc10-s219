944749903fa1ff24815dbc74c33a46b8c32808cf
Yunkai Chen <yuc331@ucsd.edu>
2019-08-26 11:39:10 -0700

    hw06

---
+++ Homeworks/hw06/tests/q5_1.py
@@ -0,0 +1,24 @@
+test = {
+  'name': 'Question 5_1',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> isinstance(ramen, Table)
+          True
+          >>> set(ramen.labels) == {'Brand', 'Variety', 'Style', 'Country', 'Stars'}
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ Homeworks/hw06/tests/q5_9.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question 5_9',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> isinstance(q5_9, int)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
